92b9d073a4d24cfa6ea4e2419a63b66c27502c37
Tyler Riedal <triedal@mail.usf.edu>
2015-02-21 23:31:49 -0500

    Code Post Cloud Pebble Crash
    This shit should work.

--- src/win_edit.c
+++ src/win_edit.c
@@ -18,11 +18,11 @@ static bool s_is_am;
 
 void win_edit_init(void)
 {
-  s_window = window_create();
+  /*s_window = window_create();
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload
-  });
+  });*/
   
   s_am_pm_window = window_create();
   window_set_window_handlers(s_am_pm_window, (WindowHandlers) {
@@ -30,7 +30,7 @@ void win_edit_init(void)
     .unload = am_pm_window_unload
   });
   
-  window_stack_push(s_window, true);
+  window_stack_push(s_am_pm_window, true);
 }
 
 static void up_down_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -57,6 +57,16 @@ void click_config_provider(void *context) {
 
 void window_load(Window *window)
 {
+  
+}
+
+void window_unload(Window *window)
+{
+  window_destroy(s_window);
+}
+
+void am_pm_window_load(Window* window)
+{
   GRect bounds = layer_get_bounds(window_get_root_layer(window));
 
   s_am_pm_actionbar = action_bar_layer_create();
@@ -67,36 +77,13 @@ void window_load(Window *window)
   text_layer_set_text_alignment(s_am_pm_textlayer, GTextAlignmentCenter);
   text_layer_set_font(s_am_pm_textlayer,fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text(s_am_pm_textlayer, "AM");
-  layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_am_pm_textlayer));
+  layer_add_child(window_get_root_layer(s_am_pm_window), text_layer_get_layer(s_am_pm_textlayer));
   s_is_am = true;
-}
-
-void window_unload(Window *window)
-{
-  window_destroy(s_window);
-}
-
-void am_pm_window_load(Window* window)
-{
-  /*GRect bounds = layer_get_bounds(window_get_root_layer(window));
-
-  s_am_pm_actionbar = action_bar_layer_create();
-  action_bar_layer_set_click_config_provider(s_am_pm_actionbar, click_config_provider);
-  action_bar_layer_set_icon(s_am_pm_actionbar,BUTTON_ID_UP,up_icon);
-  action_bar_layer_set_icon(s_am_pm_actionbar,BUTTON_ID_DOWN,down_icon);
-  action_bar_layer_set_icon(s_am_pm_actionbar,BUTTON_ID_SELECT,check_icon);
-  action_bar_layer_add_to_window(s_am_pm_actionbar,window);
-
-  s_am_pm_textlayer = text_layer_create(GRect(0, bounds.size.h/2-21, bounds.size.w-ACTION_BAR_WIDTH, bounds.size.h));
-  text_layer_set_text_alignment(s_am_pm_textlayer, GTextAlignmentCenter);
-  text_layer_set_font(s_am_pm_textlayer,fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text(s_am_pm_textlayer,s_am_pm_textbuffer);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_am_pm_textlayer));*/
 }
 
 void am_pm_window_unload(Window *window)
 {
-  /*action_bar_layer_destroy(s_am_pm_actionbar);
-  text_layer_destroy(s_am_pm_textlayer);*/
+  action_bar_layer_destroy(s_am_pm_actionbar);
+  text_layer_destroy(s_am_pm_textlayer);
   
 }
